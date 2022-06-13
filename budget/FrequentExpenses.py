3038cac09cb8e94ac27e20853958af741241ccb4
jvrmlndz <melendezdroz@gmail.com>
2022-06-13 16:51:19 -0400

    .

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -10,10 +10,8 @@ for expense in expenses.list:
     spending_categories.append(expense.category)
 
 spending_counter = collections.Counter(spending_categories)
-print(spending_counter)
 
 top5 = spending_counter.most_common(5)
-print(top5)
 
 categories, count = zip(top5)
 
