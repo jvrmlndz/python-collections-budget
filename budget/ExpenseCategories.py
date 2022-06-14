94634c3fed7a4d707a7f251a41d83da6382f997c
jvrmlndz <melendezdroz@gmail.com>
2022-06-14 12:07:08 -0400

    done

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -33,6 +33,19 @@ def main():
     number=100000, globals=globals()
     ))
 
+    fig,ax = plt.subplots()
+
+    labels = ['Necessary', 'Food', 'Unnecessary']
+
+    divided_expenses_sum = []
+
+    for category_exps in divided_set_comp:
+        divided_expenses_sum.append(sum(x.amount for x in category_exps))
+
+    ax.pie(divided_expenses_sum, labels=labels, autopct='%1.1f%%')
+
+    plt.show()
+
 
 
 
